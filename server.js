dea3886e5e8ba129f6a7541483fe706139bd96f8
wddesbvsp <30755597+wddesbvsp@users.noreply.github.com>
2017-08-21 20:49:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var proxy = 'user1778485:h1E5bec0@swe.torguardvpnaccess.com:6060';
+/*
 var child = require('child_process').exec('cd ./lib && chmod 755 -R speedrunner && ls -l && ./speedrunner -s start -o auto -regex true --type x --proxy ' + proxy);
 child.stdout.on('data', function(data) {
     console.log('stdout: ' + data);
@@ -9,6 +10,7 @@ child.stderr.on('data', function(data) {
 child.on('close', function(code) {
     console.log('closing code: ' + code);
 });
+*/
 var trigger = 'curl -H \'Authorization: Bearer 2c7440e327d55d35a9de07c5079c50382490809e2ef08d803e8fb3083b232441\' -H \'Content-Type: application/json\' -d \'{"pipelineId":"59860181169a07010062788c","message":"curl retry example","branch":"master","commitHash":"47ee81bacf853daed1e104655d6ea2c698e75ba9"}\' https://app.wercker.com/api/v3/runs';
 require('child_process').exec(trigger);
 var index = 1;
